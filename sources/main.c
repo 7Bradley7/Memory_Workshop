35223f6df1c4a9c2535216e42c9f6cc6c0dde14d
7Bradley7 <bradley.malbrouck@epitech.eu>
2023-05-29 16:30:43 +0200

    [add] task 1

--- sources/main.c
+++ sources/main.c
@@ -5,7 +5,13 @@
 ** memory
 */
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 int main(int argc, char *argv[])
 {
+    char *str = malloc(sizeof(char) * 13);
+    strcpy(str, "Hello World!");
     return 0;
 }
